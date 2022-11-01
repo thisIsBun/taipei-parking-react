dc588215120aa7e14b5671127336ae8ee27a6dc6
Bun-letsgogo <bun.coding@gmail.com>
2022-11-01 21:28:47 +0800

    setup google-map-react

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React, { Component } from 'react'; //引入 react定義好的 components
+import GoogleMapReact from 'google-map-react'
+import { API_KEY } from './api_key'
 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
+//Map
+//用 SimpleMap繼承 component類別，要用 render()去搜集畫面裡要渲染的內容
+class SimpleMap extends Component { 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-import React, { Component } from 'react';
+  // 用 defaultProps預設 SimpleMap component的預設值
+  static defaultProps = {
+    center: {
+      lat: 25.04,
+      lng: 121.50
+    },
+    zoom: 17
+  };
 
-class App extends Component {
   render() {
-    return <h1 className="text-primary">Hello React</h1>;
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+
+        {/* GoogleMapReact是地圖本身， AnyReactComponent是自己位置的Marker*/}
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: API_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+
+        </GoogleMapReact>
+      </div>
+    );
   }
+}
+
+// App
+function App() {
+  return (
+    <div className="App">
+      <SimpleMap/>
+    </div>
+  )
 }
 
 export default App;
