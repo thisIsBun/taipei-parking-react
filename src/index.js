a2b586bdc06a5d818a5c41b66bfb5ceecb760ddf
Bun-letsgogo <bun.coding@gmail.com>
2022-11-03 23:55:57 +0800

    Revert "setup my position on map"
    This reverts commit 5b05db172f1044c6d6d526879fba1945be4a6555.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
-import { Provider } from "react-redux";
-import store from "./redux/store";
-
+import reportWebVitals from './reportWebVitals';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -14,17 +11,10 @@ import store from "./redux/store";
 //   </React.StrictMode>
 // );
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    {/* 利用 Provider把 redux裡的 store值傳給 App */}
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-// reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import locationReudcer from './travelSlice'
-
-export default configureStore({
-    reducer: {
-        location: locationReudcer
-    },
-});
